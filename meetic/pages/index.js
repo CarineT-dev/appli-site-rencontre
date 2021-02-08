79ba29ffdf41957aeb6575299feeb063565999f6
CarineT-dev <74300213+CarineT-dev@users.noreply.github.com>
2021-02-08 15:56:19 +0100

    Synchronisation de la suppression

--- meetic/pages/index.js
+++ meetic/pages/index.js
@@ -1,6 +1,46 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { ProfileList } from '../components/ProfileList';
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Meetic test-entretien</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className='container d-grid gap-4'>
+        <ProfileList data='online' />
+        
+        <ProfileList data='featured' />
+      </div>
+    </div>
+  )
+}
 
 
+/*
+import Head from 'next/head';
+import { observer } from 'mobx-react';
+import Profile from '../stores/Profile';
+import { ProfileList } from '../components/ProfileList';
+const ConnectedList = observer(
+  () => <div>{
+    Profile.online.map(
+      (e, i) => <div key={i}>
+        <img src={e.picture} className="img-fluid" />
+      </div>
+    )
+  }</div>
+);
+
+const FeaturedList = observer(
+  () => <div>{
+    Profile.featured.map(
+      (e, i) => <div key={i}>
+        <img src={e.picture} className="img-fluid" />
+      </div>
+    )
+  }</div>
+);
 export default function Home() {
   return (
     <div>
@@ -9,8 +49,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <i className="bi-alarm"></i>
+        <ConnectedList />
+        <i className="bi-alarm"></i><i className="bi-alarm"></i>
+        <FeaturedList />
       </main>
     </div>
   )
 }
+
+export default= un seul element = import {Profile}
+export const = plusiers objet = import Profile
+*/
